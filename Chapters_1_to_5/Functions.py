2810c116d9403708977ee2ac52eb3d8521960653
Nicolás Ambrosis.- <57463307+nicoambrosis@users.noreply.github.com>
2022-04-07 14:11:26 -0300

    Add files via upload

---
+++ Chapters_1_to_5/Functions.py
@@ -0,0 +1,10 @@
+
+#en estas primeras lineas definimos la funcion y lo que queremos que haga
+#solo con eso la funcion no se va a ejecutar
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
+
+
+print_lyrics()
+#con esta linea lo que hacemos es llamar a la funcion y hacer que se ejecute
---
+++ Chapters_1_to_5/function_definition.py
@@ -0,0 +1,21 @@
+#este script es para definir la funcion
+#def print_lyrics():
+#    print('Pintarse la cara')
+#    print('color esperanza')
+#ahora vamos a ver como hacer para llamar/evocar la funcion
+
+#print('hellor world')
+#print('this is my favorite song')
+#print_lyrics()
+#print('whats yours')
+
+
+def pichicho(x):
+    if x=(1):
+        return('Hola')
+    elif x=(2):
+        return('bon jour')
+    else:
+        return('Hello')
+
+print(pichicho('1') , 'Glen')
